38856e1d37dda3af76fbdfeb4e60f0f653939185
vivek <codecrafter67@gmail.com>
2026-01-02 13:16:55 +0530

    implement user model and add todo interface

--- backend/src/db/mongodb.ts
+++ backend/src/db/mongodb.ts
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 
+export interface ITodo {
+    title:string;
+    isCompleted?:boolean;
+    userId:mongoose.Schema.Types.ObjectId;
+}
+
 export const connectToMongoDB = async (mongoURI: string) => {
   try {
     const connectesInstance = await mongoose.connect(mongoURI);
