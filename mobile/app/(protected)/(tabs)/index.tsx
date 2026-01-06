f2f7c7a21a6ca90e667aa74b234dfe218f5adf17
vivek <codecrafter67@gmail.com>
2026-01-06 20:45:07 +0530

    implement the searchbar component

--- mobile/app/(protected)/(tabs)/index.tsx
+++ mobile/app/(protected)/(tabs)/index.tsx
@@ -2,10 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import TodoItem from '@/components/todo-item'
+import SearchInput from '@/components/search-input'
 
 export default function todo() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{padding:10,flexDirection:"column",alignItems:"center"}}>
+      <Text style={{fontSize:25,fontWeight:"bold",color:"white",marginBottom:20}}>Todo Master</Text>
+      <SearchInput/>
       <TodoItem/>
     </SafeAreaView>
   )
---
+++ mobile/components/search-input.tsx
@@ -0,0 +1,27 @@
+import { View, Text, TextInput, Pressable } from 'react-native'
+import React, { useState } from 'react'
+
+export default function SearchBar() {
+    const [todo, setTodo] = useState("");
+    return (
+        <View style={{ width: "100%",   marginBottom: 40, flexDirection: "row", gap: 5,justifyContent:"center",alignItems:"center"}}>
+            <TextInput style={{ width: "70%", fontSize: 15,padding:15 ,borderRadius: 10,backgroundColor: "#212121"}}
+                placeholder='Enter Todo...'
+                placeholderTextColor={"grey"}
+                value={todo}
+                onChangeText={(text) => setTodo(text)}
+            />
+            <Pressable style={{
+                height: 50,
+                width: "20%",
+                backgroundColor: "#4F46E5",
+                borderRadius: 8,
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+                <Text style={{color:"white",fontSize:15}}>Add</Text>
+            </Pressable>
+        </View>
+
+    )
+}
